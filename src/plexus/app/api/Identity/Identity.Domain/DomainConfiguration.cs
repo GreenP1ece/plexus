6259813142dc4d7d8efc724a0e96bafcb6cd7964
GreenP1ece <kirya_kravchenko_2020@mail.ru>
2026-06-28 17:06:48 +0300

    feat(Identity): adding Identity

---
+++ src/plexus/app/api/Identity/Identity.Domain/DomainConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Common.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Domain;
+
+public static class DomainConfiguration
+{
+    public static IServiceCollection AddIdentityDomain(
+        this IServiceCollection services)
+        => services
+            .AddCommonDomain(
+                Assembly.GetExecutingAssembly());
+}
